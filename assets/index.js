fb6dd86331ead902d7c5a0743e6af666221a8f43
Jake Schroeder <jakeschroeder8@gmail.com>
2019-02-06 03:01:11 -0600

    added other 2 animations and made body flex

--- assets/index.js
+++ assets/index.js
@@ -5,3 +5,19 @@ var animation = bodymovin.loadAnimation({
     autoplay: true,
     path: './data/data.json'
   })
+
+  var animation1 = bodymovin.loadAnimation({
+    container: document.getElementById('bx'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './data/data-best.json'
+  })
+
+  var animation2 = bodymovin.loadAnimation({
+    container: document.getElementById('bz'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './data/data-best-2.json'
+  })
